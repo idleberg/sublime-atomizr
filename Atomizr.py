9c0c67d6aea41b44f8a8026a6b7cc893c69e6277
Jan T. Sott <jan@idleberg.com>
2016-02-08 18:12:01 +0100

    add AtomToAtom & AtomToSubl commands

--- Atomizr.py
+++ Atomizr.py
@@ -1,6 +1,7 @@
 import sublime, sublime_plugin
 
-class ToAtomCommand(sublime_plugin.TextCommand):
+# Converts Sublime Text completions into Atom snippets
+class SublToAtomCommand(sublime_plugin.TextCommand):
 
     def run(self, edit):
         import json
@@ -10,7 +11,7 @@ class ToAtomCommand(sublime_plugin.TextCommand):
         try:
             data = json.loads(selection)
         except ValueError:
-            sublime.error_message("No valid JSON")
+            sublime.error_message("Error: No valid JSON")
 
         scope = "." + data['scope']
         completions = data['completions']
@@ -27,3 +28,37 @@ class ToAtomCommand(sublime_plugin.TextCommand):
 
         selection = sublime.Region(0, self.view.size())
         self.view.replace(edit, selection, json.dumps(atom, sort_keys=True, indent=4, separators=(',', ': ')))
+
+
+# TODO: Converts Atom snippets into Sublime Text completions
+class AtomToSublCommand(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        import cson, json
+        
+        selection = self.view.substr(sublime.Region(0, self.view.size()))
+
+        try:
+            data = cson.loads(selection)
+        except:
+            sublime.error_message("Error: No valid CSON")
+
+        print("\nNot yet implemented, printing JSON instead:")
+        print(json.dumps(data))
+
+
+# Converts Atom snippets (CSON into JSON)
+class AtomToAtomCommand(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        import cson, json
+        
+        selection = self.view.substr(sublime.Region(0, self.view.size()))
+
+        try:
+            data = cson.loads(selection)
+        except:
+            sublime.error_message("Error: No valid CSON")
+
+        selection = sublime.Region(0, self.view.size())
+        self.view.replace(edit, selection, json.dumps(data, sort_keys=False, indent=4, separators=(',', ': ')))
